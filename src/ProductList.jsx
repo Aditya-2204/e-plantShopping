332130d8541c0b3b91f7689ad23b6ca94228551a
Aditya-2204 <william1binki@gmail.com>
2025-05-31 20:45:14 -0500

    Revert "Did Step 5"
    This reverts commit d9a275945663a3b9ce11f7a51f9897e71b3592ca.

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,24 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
-import { useDispatch } from 'react-redux';
-import { addItem } from './CartSlice';
-
 function ProductList({ onHomeClick }) {
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
 
     const [addedToCart, setAddedToCart] = useState({});
-
-    const handleAddToCart = (plant) => {
-        dispatch(addItem(plant));
-
-        setAddedToCart((prevState) => ({
-            ...prevState,
-            [product.name]: true,
-          }));
-    }
-
 
     const plantsArray = [
         {
